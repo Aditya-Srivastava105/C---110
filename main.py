7e0a125d306f4d2a9dc25177d651ab024e5298f7
Aditya-Srivastava105 <71447941+Aditya-Srivastava105@users.noreply.github.com>
2021-12-02 09:00:35 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+import pandas as pd
+import random
+import statistics
+import csv
+
+df = pd.read_csv('medium_data.csv')
+data = df['reading_time'].tolist()
+fig = ff.create_distplot([data], ['reading_time'], show_hist = False)
+fig.show()
+
+population_mean = statistics.mean(data)
+print("Mean = ", population_mean)
+population_standarddeviation = statistics.stdev(data)
+print("Standard deviation = ", population_standarddeviation)
+
+dataset = []
+for i in range(0,1000):
+  random_index = random.randint(0,len(data))
+  value = data[random_index]
+  dataset.append(value)
+
+mean = statistics.mean(dataset)
+standarddeviation = statistics.stdev(dataset)
+print("mean of 1000 values = ", mean)
+print("standard deviation of 1000 values = ", standarddeviation)
